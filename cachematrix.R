c6cc237f9600e418dc1f0d00898e0cde7ecf29e0
brnoxx <haydenshimizu@gmail.com>
2014-10-24 15:25:07 -0700

    First Upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+### makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(x = matrix()) { ## x should be an invertible square matrix
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m ##prints NULL until cacheSolve is called
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { ##input the special stored matrix
+  m <- x$getinverse()
+  if(!is.null(m)) { ##if cached, will return m
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()  ##else the inverse is solved and stored
+  m <- solve(data)
+  x$setinverse(m)
+  m
 }
+
+##sample output
+c<-matrix(c(10,20,30,40),2,2) ##create invertible square matrix
+a <- makeCacheMatrix(c) ##create special matrix
+a$get()  ##print matrix
+a$getinverse()  ##retrieve NULL, as cacheSolve hasn't been called on "a"
+cacheSolve(a)  ##call for the first time, solve is run and inverse is cached
+a$getinverse() ##retrieve the cached inverse
